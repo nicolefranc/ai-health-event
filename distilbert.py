130c7e8f7de4901bba212c93e7929641ff50a064
Nicole <nicoleyu@outlook.sg>
2022-06-20 02:10:00 +0800

    Change distilbert implementation

--- distilbert.py
+++ distilbert.py
@@ -1,41 +1,68 @@
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+# from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-import numpy as np
-import torch
+# import numpy as np
+# import torch
+import math
+import requests
 
-device = torch.device('cpu')
+
+def predict():
+
+    API_URL = "https://api-inference.huggingface.co/models/dibsondivya/distilbert-phmtweets-sutd"
+    headers = {"Authorization": "Bearer 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"}
+
+    def query(payload):
+        response = requests.post(API_URL, headers=headers, json=payload)
+        return response.json()
+
+    output = query({
+        "inputs": "I like you. I love you",
+    })[0]
+    # print(output)
+
+    max_score = max(label['score'] for label in output)
+
+    for result in output:
+        l = result['label']
+        s = result['score']
+        if s == max_score:
+            pred = l[-1]
+
+    # print(pred)
+    return pred
 
 
-def predict(tweet):
+# def predict1(tweet):
+#     device = torch.device('cpu')
 
-    tokenizer = AutoTokenizer.from_pretrained(
-        'dibsondivya/distilbert-phmtweets-sutd')
+#     tokenizer = AutoTokenizer.from_pretrained(
+#         'dibsondivya/distilbert-phmtweets-sutd')
 
-    model = AutoModelForSequenceClassification.from_pretrained(
-        'dibsondivya/distilbert-phmtweets-sutd')
+#     model = AutoModelForSequenceClassification.from_pretrained(
+#         'dibsondivya/distilbert-phmtweets-sutd')
 
-    # Tokenize inputs
-    inputs = tokenizer(tweet, padding=True, truncation=True, return_tensors="pt").to(
-        device)  # Move the tensor to the GPU
+#     # Tokenize inputs
+#     inputs = tokenizer(tweet, padding=True, truncation=True, return_tensors="pt").to(
+#         device)  # Move the tensor to the GPU
 
-    # Inference model and get logits
-    outputs = model(**inputs)
+#     # Inference model and get logits
+#     outputs = model(**inputs)
 
-    # get predictions for test data
-    with torch.no_grad():
-        predictions = torch.nn.functional.softmax(outputs.logits, dim=-1)
-        preds = predictions.detach().cpu().numpy()
+#     # get predictions for test data
+#     with torch.no_grad():
+#         predictions = torch.nn.functional.softmax(outputs.logits, dim=-1)
+#         preds = predictions.detach().cpu().numpy()
 
-    print(preds)
-    # model's performance
-    preds = np.argmax(predictions)
-    # print(classification_report(test_y, preds))
+#     print(preds)
+#     # model's performance
+#     preds = np.argmax(predictions)
+#     # print(classification_report(test_y, preds))
 
-    # model's performance
-    preds = np.argmax(predictions)
-    # print(classification_report(test_y, preds))
+#     # model's performance
+#     preds = np.argmax(predictions)
+#     # print(classification_report(test_y, preds))
 
-    return preds.item()
+#     return preds.item()
 
 # pred = predict("Alzheimer's is the worst disease on the planet")
 # print(pred)
@@ -52,3 +79,6 @@ def convert_label(label):
         return 'OTHER MENTION'
     elif label == 3:
         return 'SELF-MENTION'
+
+
+print(predict())
